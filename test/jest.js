d67d0d76c1913fade76647be572fbee7c37c3d9c
Magdi Tiea <magditiea@gmail.com>
2021-03-13 03:16:16 -0500

    passport, passport-local modules added

--- test/jest.js
+++ test/jest.js
@@ -1,5 +1,6 @@
 const userRouter = require('../routes/api-routes');
 const taskRouter = require('../routes/html-routes');
+const express = require('express')
 const app = express();
 app.use(express.json());
 app.use(userRouter);
