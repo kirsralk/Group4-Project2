e75164372f3d2b940dc3960b64eb9b523db42c6c
Kirsi Ralko <kirsiralko@gmail.com>
2021-03-13 09:14:12 -0600

    Update api-routes.js

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -6,7 +6,7 @@ module.exports = function(app) {
   // Using the passport.authenticate middleware with our local strategy.
   // If the user has valid login credentials, send them to the members page.
   // Otherwise the user will be sent an error
-
+​
   app.post("/api/login", passport.authenticate("local"), (req, res) => {
     // Sending back a password, even a hashed password, isn't a good idea
     res.json({
@@ -14,40 +14,36 @@ module.exports = function(app) {
       id: req.user.id
     });
   });
-
+​
   // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
   // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
   // otherwise send back an error
   app.post("/api/signup", (req, res) => {
-    console.log(req.body);
     db.User.create({
       name: req.body.name,
       email: req.body.email,
-      password: req.body.password,
-      pokemon1: 1
+      password: req.body.password
     })
       .then(() => {
         mailSend(req);
-        // partyCreate(res);
+        partyCreate(res);
         res.redirect(307, "/api/login");
       })
       .catch(err => {
         res.status(401).json(err);
       });
   });
-
-
+  function partyCreate(res) {
+    // db.Party.create({
+​
+    // });
+    console.log(res.req.body.id);
+  }
+​
   function mailSend(req) {
     const output = `
                 <p>You have signed up for Pokemon Stay!!</p>
                 <h3>Pokemon Stay Account </h3>
-
- });
-
-  function mailSend(req) {
-    const output = `
-                <p>You have signed up for Pokemon STAY!!</p>
-                <h3>Your Account:</h3>
                 <ul>  
                   <li>Username: ${req.body.name}</li>
                   <li>Email: ${req.body.email}</li>
@@ -55,9 +51,6 @@ module.exports = function(app) {
                 </ul>
                 <h3>Message</h3>
                 <p>Welcome to Pokemon Stay! It's time to choose your pokemon! Your adventure awaits.</p>
-
-                <p>Welcome to Pokemon STAY! It's time to choose your pokemon! Your adventure awaits.</p>
-
               `;
     // create reusable transporter object using the default SMTP transport
     const transporter = nodemailer.createTransport({
@@ -72,23 +65,16 @@ module.exports = function(app) {
         rejectUnauthorized: false
       }
     });
-
+​
     // setup email data with unicode symbols
     const mailOptions = {
-
       from: "'Pokemon Stay' <tester9191@hotmail.com>", // sender address
       to: req.body.email, // list of receivers
       subject: "New Pokemon Stay Account", // Subject line
       text: "Welcome to Pokemon Stay!", // plain text body
-
-      from: "'Pokemon Stay' <wallace.ryan50@ethereal.email>", // sender address
-      to: req.body.email, // list of receivers
-      subject: "New Pokemon STAY Account", // Subject line
-      text: "Welcome to Pokemon STAY!", // plain text body
-
       html: output // html body
     };
-
+​
     // send mail with defined transport object
     transporter.sendMail(mailOptions, (error, info) => {
       if (error) {
@@ -96,17 +82,17 @@ module.exports = function(app) {
       }
       console.log("Message sent: %s", info.messageId);
       console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-
+​
       res.render("contact", { msg: "Email has been sent" });
     });
   }
-
+​
   // Route for logging user out
   app.get("/logout", (req, res) => {
     req.logout();
     res.redirect("/");
   });
-
+​
   // Route for getting some data about our user to be used client side
   app.get("/api/user_data", (req, res) => {
     if (!req.user) {
@@ -118,8 +104,7 @@ module.exports = function(app) {
       res.json({
         name: req.user.name,
         email: req.user.email,
-        id: req.user.id,
-        pokemon1: req.user.pokemon1
+        id: req.user.id
       });
     }
   });
